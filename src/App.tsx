d2a3a72a62a2210dc88bd7dd207e878d48d21a96
Farhan Husyen Ramadhan <122083217+farhanhr@users.noreply.github.com>
2025-07-28 05:09:43 +0700

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import Navbar from "./components/Navbar"
+
+function App() {
+
+  return (
+    <>
+    <div className="container mx-auto px-4">
+    <Navbar />
+    {/* <h1 className="font-bold m-10 text-6xl text-red-500">Hello</h1> */}
+    </div>
+    </>
+  )
+}
+
+export default App
---
+++ src/components/Navbar.tsx
@@ -0,0 +1,20 @@
+
+const Navbar = () => {
+  return (
+    <div className="navbar">
+        <div className="logo">
+            Portofolio
+        </div>
+        <ul className="menu">
+            <li>
+                <a href="#">Home</a>
+                <a href="#">About</a>
+                <a href="#">Projects</a>
+                <a href="#">Contact</a>
+            </li>
+        </ul>
+    </div>
+  )
+}
+
+export default Navbar
